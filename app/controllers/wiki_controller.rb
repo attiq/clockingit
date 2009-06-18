c502af4ed07bcebd1545aefb81b2475b39f87293
attiq <attiq_attiq@yahoo.com>
2009-06-18 09:31:59 +0500

    issues resolved

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -22,7 +22,7 @@ class WikiController < ApplicationController
     @wikis = []
     project_ids.each{ |id|
       @project = Project.find_by_id(id)
-      unless @project.wiki_pages.empty?
+      unless @project.wiki_pages.nil?
       @project.wiki_pages.each{ |w|
         @wikis.push(WikiPage.find_by_id(w.id)) 
       }
